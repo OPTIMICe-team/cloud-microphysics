bbf069232fe9b663224e280033594cd5dcbedf4e
Davide Ori <dori@uni-koeln.de>
2020-08-04 00:11:40 +0200

    some progress in making everything more clean

--- P3/scattering_functions.py
+++ P3/scattering_functions.py
@@ -28,13 +28,31 @@ scat_cloud = pd.DataFrame(index=Dcloud, columns=[w for w in waves.keys()])
 scat_rain = pd.DataFrame(index=Drain, columns=[w for w in waves.keys()])
 for w in waves.keys():
 	n_water = refractive.m_w_0C[waves[w]]
-	for i, d in enumerate(Dcloud):
-		scat_cloud[i, w] = tm_reflectivity(d, 1.0e-3*waves[w], n_water)
-	for i, d in enumerate(Drain):
-		scat_rain[i, w] = tm_reflectivity(d, 1.0e-3*waves[w], n_water)
+	for d in Dcloud:
+		scat_cloud.loc[d, w] = tm_reflectivity(d, 1.0e-3*waves[w], n_water)
+	for d in Drain:
+		scat_rain.loc[d, w] = tm_reflectivity(d, 1.0e-3*waves[w], n_water)
 
 
 def calc_liquid_Z(N0, mu, lam, D, z):
 	N = gammaPSD(N0, mu, lam)
-    return (N(D)*z*np.gradient(D)).sum()
+	return (N(D)*z*np.gradient(D)).sum()
 vector_liquid_Z = np.vectorize(calc_liquid_Z, excluded=['D', 'z'], otypes=[np.float])
+
+
+z_tm_solid_sphere = pd.DataFrame(index=Dice, columns=[w for w in waves.keys()])
+z_tm_BF95_spheroid = pd.DataFrame(index=Dice, columns=[w for w in waves.keys()])
+BF95density = lambda x, ar: np.minimum(0.0121*6.0*x**(-1.1)/(np.pi*ar), 900.0)
+for w in waves.keys():
+	wl = waves[w]
+	for d in Dice:
+		z_tm_solid_sphere.loc[d, w] = tm_reflectivity(d, 1.0e-3*wl, refractive.mi(wl, 0.900), 1.0)
+		z_tm_BF95_spheroid.loc[d, w] = tm_reflectivity(d, 1.0e-3*wl, refractive.mi(wl, 1.0e-3*BF95density(d, 0.6)), 0.6)
+
+a_graupel = np.linspace(21, 472, 1000)
+a_partrimed = np.linspace(0.0121, 0.024, 1000)
+
+#z_tm_graupel = pd.read_csv('z_tm_graupel_'+freq_str+'.csv', index_col=0, dtype=np.float64, engine='c')
+#z_tm_graupel.columns = z_tm_graupel.columns.astype(np.float)
+#z_tm_partrimed = pd.read_csv('z_tm_partrimed_'+freq_str+'.csv', index_col=0, dtype=np.float64, engine='c')
+#z_tm_partrimed.columns = z_tm_partrimed.columns.astype(np.float)
