eb2b9ffc5daa1c9480d09073db3b9df409252dab
Markus Karrer <markus.karrer@uni-koeln.de>
2020-11-12 14:20:22 +0100

    update run_colleffi_check_in_McSnow.sh

--- run_colleffi_check_in_McSnow.sh
+++ run_colleffi_check_in_McSnow.sh
@@ -9,4 +9,7 @@ cd /home/mkarrer/Dokumente/McSnow_checks/run
 
 #plot the collision efficiency and integrate numerically and compare it with the parameterization in the SB scheme
 cd /home/mkarrer/Dokumente/cloud-microphysics/
-python bulk_colleffi_riming.py
+python bulk_colleffi_riming.py -col 1 -m 0 -fr 0
+python bulk_colleffi_riming.py -col 1 -m 1 -fr 0
+python bulk_colleffi_riming.py -col 1 -m 0 -fr 1
+python bulk_colleffi_riming.py -col 1 -m 1 -fr 1
