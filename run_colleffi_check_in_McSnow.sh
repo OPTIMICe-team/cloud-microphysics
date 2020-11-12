3ffed553e007b1565def7cf6a6a0c89e1a096eda
Markus Karrer <markus.karrer@uni-koeln.de>
2020-11-12 14:43:39 +0100

    uncomment compile&run of McSnow in run_colleffi_check_in_McSnow.sh

--- run_colleffi_check_in_McSnow.sh
+++ run_colleffi_check_in_McSnow.sh
@@ -1,11 +1,11 @@
 set -e
 #compile
 cd /home/mkarrer/Dokumente/McSnow_checks
-#make release
+make release
 
 #check
 cd /home/mkarrer/Dokumente/McSnow_checks/run
-#bash check
+bash check
 
 #plot the collision efficiency and integrate numerically and compare it with the parameterization in the SB scheme
 cd /home/mkarrer/Dokumente/cloud-microphysics/
